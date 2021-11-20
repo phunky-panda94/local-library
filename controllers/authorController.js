85055cb4d740869ccfdabe3ca31bc59cdca968e4
phunky-panda94 <victor.nim94@outlook.com>
2021-11-20 18:41:55 +1100

    added controllers

---
+++ controllers/authorController.js
@@ -0,0 +1,15 @@
+const Author = require('../models/author');
+
+// display list of authors
+
+// display author detail page
+
+// display create new author form
+
+// handle create new author
+
+// handle delete author
+
+// display update author form
+
+// handle update author
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Local Library' });
 });
+
+// catalog page
+router.get('/catalog', (req, res, next) => {
+    res.send('Catalog page');
+})
 
 module.exports = router;
